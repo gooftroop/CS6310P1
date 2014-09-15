78daa7504144ff40bf4ef09a27a9b66024bb3037
Brandon Webb <webb.c.brandon@gmail.com>
2014-09-15 18:28:46 -0400

    attempt to sync

---
+++ tpdahp/tpdahp/Demo.java
@@ -0,0 +1,62 @@
+package tpdahp;
+
+import java.util.Hashtable;
+
+import lib.ISimulation;
+
+public class Demo {
+	
+	private static final Hashtable<String, String> options = new Hashtable<String, String>();
+	
+	public static void main(String...args) {
+		Demo.configureOpts();
+		Demo.parseArgs(args);
+		
+		int width = Integer.parseInt(options.get("-d"));
+		int height = Integer.parseInt(options.get("-d"));
+		float left = Float.parseFloat(options.get("-l"));
+		float right = Float.parseFloat(options.get("-r"));
+		float top = Float.parseFloat(options.get("-t"));
+		float bottom = Float.parseFloat(options.get("-b"));
+		
+		ISimulation simulation = new Tpdahp(new TpdahpHandler(width, height));	
+		simulation.setup(width, height, left, right,top, bottom);
+		
+		System.out.format("Starting %d x %d plate simulation...\n", width, height);
+		simulation.run();
+	}
+
+	private static void configureOpts() {
+		options.put("-d", "");
+		options.put("-l", "");
+		options.put("-r", "");
+		options.put("-t", "");
+		options.put("-b", "");
+	}
+	
+	private static void parseArgs(String... args) {
+		
+		String curr = "", currOpt = "";
+		
+		if (args.length == 0) throw new IllegalArgumentException("No arguments provided to Demo");
+		
+		for (int i = 0; i < args.length; i++) {
+			
+			curr = args[i];
+	        switch (curr.charAt(0)) {
+	        
+	        	case '-':
+		            if (!"".equals(currOpt)) throw new IllegalArgumentException("No value for argument '" + currOpt + "'");
+		            if (args.length == (i + 1))  throw new IllegalArgumentException("Expected value after argument '" + curr + "'");
+		            if (!options.containsKey(curr)) throw new IllegalArgumentException("Invalid argument '" + curr + "'");
+		            currOpt = curr;
+		            break;
+		        default:
+		        	if ("".equals(currOpt)) throw new IllegalArgumentException("Expected argument for value '" + curr + "'");
+		            options.put(currOpt, curr);
+		            currOpt = "";
+		            break;
+	        }
+		}	
+	}
+}
---
+++ tpdahp/tpdahp/Tpdahp.java
@@ -0,0 +1,116 @@
+package tpdahp;
+
+import lib.IResultsHandler;
+import lib.Simulation;
+
+public final class Tpdahp extends Simulation {
+	
+	private double oldMatrix[][];
+	private double newMatrix[][];
+	
+	private double leftTemp, rightTemp, topTemp, bottomTemp;
+	
+	public Tpdahp(IResultsHandler rh) {
+		super(rh);
+	}
+	
+	@Override
+	public void setup(int height, int width, float leftTemp, float rightTemp, float topTemp, float bottomTemp) {
+		
+		if (height < 0 || height >= Integer.MAX_VALUE)
+			throw new IllegalArgumentException("Invalid height dimension");
+		
+		if (width < 0 || width >= Integer.MAX_VALUE)
+			throw new IllegalArgumentException("Invalid width dimension");
+		
+		if (leftTemp < MIN_TEMP || leftTemp > MAX_TEMP)
+			throw new IllegalArgumentException("Invalid left temperature value");
+		
+		if (rightTemp < MIN_TEMP || rightTemp > MAX_TEMP)
+			throw new IllegalArgumentException("Invalid left temperature value");
+		
+		if (topTemp < MIN_TEMP || topTemp > MAX_TEMP)
+			throw new IllegalArgumentException("Invalid left temperature value");
+		
+		if (bottomTemp < MIN_TEMP || bottomTemp > MAX_TEMP)
+			throw new IllegalArgumentException("Invalid left temperature value");
+		
+		this.height = height + 2;
+		this.width = width + 2;
+		
+		this.leftTemp 	= (double) leftTemp;
+		this.rightTemp 	= (double) rightTemp;
+		this.topTemp 	= (double) topTemp;
+		this.bottomTemp = (double) bottomTemp;
+		
+		this.initializePlate();
+	}
+	
+	@Override
+	protected void initializePlate() {
+		
+		oldMatrix = new double[this.width][this.height];
+		newMatrix = new double[this.width][this.height];
+		
+		initializeMatrix(oldMatrix);
+		initializeMatrix(newMatrix);
+	}
+	
+	@Override
+	public void run() {
+		
+		float maxDeviation = 0.0f;
+		
+		do {
+			
+			maxDeviation = 0.0f;
+			
+			// iterate through newMatrix, filling in values from oldMatrix, recoding maxDeviation;
+			for ( int i = 1 ; i < this.width - 1 ; i ++ ) {
+				for ( int j = 1; j < this.height - 1 ; j++ ) {
+				
+					newMatrix[i][j] = ( ( oldMatrix[i-1][j] + oldMatrix[i+1][j] + oldMatrix[i][j-1] + oldMatrix[i][j+1] ) / 4.0f );
+					
+					if ( newMatrix[i][j] - oldMatrix[i][j] > maxDeviation ) {
+						maxDeviation = (float) (newMatrix[i][j] - oldMatrix[i][j]);
+					}
+					
+					this.update((float) newMatrix[i][j], i, j);
+				}
+			}
+
+			// overwrite oldMatrix with newMatrix
+			swapMatrixes();
+			
+		} while (maxDeviation >= 0.01 && numIterations++ <= MAX_ITERATIONS);
+	}
+	
+	private void initializeMatrix(double matrix[][]) {
+		
+		for (int i = 0; i < this.width; i++) {
+			for ( int j = 0; j < this.height; j++) {
+				
+				// the corners of the matrix are are not used in the simulation, and so do not need special cases
+				if (i == 0)
+					matrix[i][j] = this.topTemp; 	// top row
+				else if (i == (this.width - 1))
+					matrix[i][j] = this.bottomTemp; // bottom row
+				else if ( j == 0 )
+					matrix[i][j] = this.leftTemp; 	// left column 
+				else if (j == (this.height - 1))
+					matrix[i][j] = this.rightTemp; 	// right column 
+				else
+					matrix[i][j] = 0.0f; 			// not an edge, the plate itself
+			}
+		}
+	}
+	
+	private void swapMatrixes() {
+		
+		for ( int i = 1 ; i < this.width - 1 ; i++ ) {
+			for ( int j = 1 ; j < this.height - 1 ; j++ ) {
+				oldMatrix[i][j] = newMatrix[i][j];
+			}
+		}
+	}
+}
